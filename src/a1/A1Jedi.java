8724243fa9810ca35179be2ee83ad947d22ebc13
charlietran <charlietran@152.23.230.244>
2019-09-01 23:51:59 -0400

    YESS, completed A1 Jedi. Feels good to be a Jedi.

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,84 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		// takes in an integer to show the number of items in the store.
+				int itemCount = scan.nextInt();
+				
+				// an array to take in the name of each item in the store
+				String[] itemNames = new String[itemCount];
+				
+				// an array to take in the prices of each item in the store
+				double[] itemPrice = new double[itemCount];
+				
+				// a for loop to assign price values into the price array
+				for(int i=0; i<itemPrice.length; i++) {
+					itemNames[i] = scan.next();
+					itemPrice[i] = scan.nextDouble();
+				}
+				
+				// takes in an integer to show the number of customers
+				int totalCust = scan.nextInt();
+				
+				// an array to take in the first names of customers
+				String[] firstNames = new String[totalCust];
+				
+				// an array to take in the last names of customers
+				String [] lastNames = new String[totalCust];
+				
+				// an array to take in the total number of items bought by the customer
+				int[] totalItems = new int[totalCust];
+				
+				
+				
+				int[] itemCounter = new int[itemCount];
+				int[] custCounter = new int[itemCount];
+				
+				// a for loop to read and input values into each array listed above
+				for(int i=0; i<firstNames.length; i++) {
+					firstNames[i] = scan.next();
+					lastNames[i] = scan.next();
+					totalItems[i] = scan.nextInt();
+					// a nested for loop for each item to store the quantity and price of each item into an array
+					for (int k=0; k<totalItems[i]; k++) {
+						int[] itemQuant = new int[totalItems[i]];
+						itemQuant[k] = scan.nextInt();
+						String[] shoppingList = new String [totalItems[i]];
+						shoppingList[k] = scan.next();
+						boolean[] checkCust = new boolean[itemCount];
+						// another nested for loop to check if a customer has bought a specific item
+						for (int m=0; m<itemNames.length; m++) {
+							int counter = 0;
+							if(itemNames[m].equals(shoppingList[k])) {
+								counter ++;
+							}
+							if (counter > 0) {
+								checkCust[m] = true; 
+							} else {
+								checkCust[m] = false;
+							}
+							log(itemQuant[k], shoppingList[k], itemNames, itemCounter, custCounter, checkCust);
+						}
+					
+					} 
+				}	scan.close();
+				
+				for(int i=0; i<itemNames.length; i++) {
+					if (custCounter[i] < 1) {
+					System.out.println("No customers bought " + itemNames[i]);
+					} else {
+						System.out.println(custCounter[i] + " customers bought " + itemCounter[i] + " " + itemNames[i]);
+					}
+				}
+	}
+	
+	static void log(int itemQuant, String shoppingItem, String[] itemNames, int[] itemCounter, int[] custCounter, boolean[] checkCust) {
+		for (int i=0; i<itemNames.length; i++) {
+			if (itemNames[i].equals(shoppingItem) && checkCust[i] == false) {
+				custCounter[i] ++;
+			}
+			if (itemNames[i].equals(shoppingItem)) {
+				itemCounter[i] = itemQuant;
+			}
+		}
 	}
 }
